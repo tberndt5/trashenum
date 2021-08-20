6c63fdf2c75f92b1a009c59c8d4eec6e8b544c6f
tberndt5 <39279867+tberndt5@users.noreply.github.com>
2021-08-20 09:32:41 -0500

    Added a few more trashy lines

--- trashenum.sh
+++ trashenum.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run nmap to get open ports and store in namp.txt
+echo "$(tput setaf 10)Running nmap scan...$(tput sgr 0)"
 nmap -p- -T4 $1 > nmap.txt
 # Run through nmap.txt for open ports and store the ports in open_ports.txt
 while IFS= read -r line; do
@@ -13,11 +14,27 @@ while IFS= read -r port; do
                 echo "$(tput setaf 3)FTP Port open..$(tput sgr 0)"
                 echo "$(tput setaf 4)Connecting to ftp with anonymous$(tput sgr 0)"
                 # ftp command
+                curl ftp://$1 --user anonymous:anonymous
+	elif [[ $port -eq 22 ]]
+	then
+		echo "$(tput setaf 3)SSHPort open...$(tput sgr 0)"
+		echo "$(tput setaf 8)Attempting to grab banner.$(tput sgr 0)"
+		#nc -v $1 22
         elif [[ $port -eq 80 ]]
         then
                 echo "$(tput setaf 1)HTML Port open...$(tput sgr 0)"
                 echo "$(tput setaf 2)Running go buster...$(tput sgr 0)"
-                #gobuster command
+                gobuster dir -u $1 -w /usr/share/wordlists/dirbuster/directory-list-1.0.txt > gobuster.txt
+                echo "$(tput setaf 6)Gobuster scan complete!$(tput sgr 0)"
+		echo "$(tput setaf 9)Directories found$(tput sgr 0)"
+		echo "------------------------------"
+		while IFS= read -r line; do
+        		echo $line | grep "(Status:"
+		done < "gobuster.txt"
+		echo "-----------------------------"
+                echo "$(tput setaf 7)Running nikto against $1...$(tput sgr 0)"
+                nikto -h http://$1 > nikto.txt
+                echo "$(tput setaf 8)Nikto scan complete!$(tput sgr 0)"
         else
                 echo "$(tput setaf 5)Other interesing port found: $port$(tput sgr 0)"
                 #send interesting port to file
